28c23ec37c0b1778688704bb7de4e2429c43f01b
Tamar Auber <tamarauber@Tamars-MacBook-Pro.local>
2020-02-05 23:29:49 -0500

    First version

---
+++ MemeMeDraft/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MemeMeDraft
+//
+//  Created by Tamar Auber on 2/5/20.
+//  Copyright © 2020 Tamar Auber. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate,
+UINavigationControllerDelegate,
+UITextFieldDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
